8636b98cb4e305593b5d3f34177663c8208dfd10
Anna Penta <apenta@kent.edu>
2018-03-14 10:35:27 -0400

    home index added

---
+++ NPGeek.Web/DAL/IParkDAL.cs
@@ -0,0 +1,15 @@
+﻿using NPGeek.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPGeek.Web.DAL
+{
+    interface IParkDAL
+    {
+        List<ParkModel> GetAllParks();
+        
+    }
+}
---
+++ NPGeek.Web/DAL/ParkSqlDAL.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using NPGeek.Web.Models;
+
+namespace NPGeek.Web.DAL
+{
+    public class ParkSqlDAL : IParkDAL
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        public List<ParkModel> GetAllParks()
+        {
+            List<ParkModel> parks = new List<ParkModel>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("", conn);
+                }
+            }
+            catch (SqlException)
+            {
+
+            }
+            return parks;
+        }
+    }
+}
